22f3ccf7a04bec59e4270c4bb67997393607d25f
erminmaksumic-e2x <erminmaksumic@e2x.com>
2023-07-05 23:36:47 +0200

    First good version

--- Wumpus js.py
+++ Wumpus js.py
@@ -1,131 +1,110 @@
 import random
 
-ROWS = 5
-COLS = 6
+class WumpusGame:
+    def __init__(self, size=6):
+        self.size = size
+        self.grid = [[[] for _ in range(size)] for _ in range(size)]
+        self.agent_pos = (0, 0)
+        self.wumpus_pos = None
+        self.pit_pos = None
+        self.gold_pos = None
 
-WUMPUS = (random.randint(0, ROWS-1), random.randint(0, COLS-1))
-GOLD = (random.randint(0, ROWS-1), random.randint(0, COLS-1))
-PIT1 = (random.randint(0, ROWS-1), random.randint(0, COLS-1))
-PIT2 = (random.randint(0, ROWS-1), random.randint(0, COLS-1))
+    def initialize_game(self):
+        self.agent_pos = (0, 0)
+        self.wumpus_pos = self.generate_random_position()
+        self.pit_pos = self.generate_random_position()
+        self.gold_pos = self.generate_random_position()
+        while self.wumpus_pos == self.pit_pos or self.wumpus_pos == self.gold_pos or self.pit_pos == self.gold_pos:
+            self.pit_pos = self.generate_random_position()
+            self.gold_pos = self.generate_random_position()
 
-class WumpusWorld:
-    def __init__(self):
-        self.grid = [[' ' for _ in range(COLS)] for _ in range(ROWS)]
-        self.agent_location = (0, 0)
-        self.agent_alive = True
-        self.agent_won = False
-        self.game_over = False
+    def generate_random_position(self):
+        return random.randint(0, self.size - 1), random.randint(0, self.size - 1)
 
-    def print_grid(self):
-        for row in self.grid:
-            print('+---+---+---+---+---+---+')
-            print('| {} | {} | {} | {} | {} | {} |'.format(*row))
-        print('+---+---+---+---+---+---+')
+    def get_adjacent_cells(self, pos):
+        x, y = pos
+        adjacent_cells = []
+        if x > 0:
+            adjacent_cells.append((x - 1, y))
+        if x < self.size - 1:
+            adjacent_cells.append((x + 1, y))
+        if y > 0:
+            adjacent_cells.append((x, y - 1))
+        if y < self.size - 1:
+            adjacent_cells.append((x, y + 1))
+        return adjacent_cells
 
-    def update_grid(self):
-        self.grid = [[' ' for _ in range(COLS)] for _ in range(ROWS)]
-        self.grid[WUMPUS[0]][WUMPUS[1]] = 'W'
-        self.grid[GOLD[0]][GOLD[1]] = 'G'
-        self.grid[PIT1[0]][PIT1[1]] = 'P'
-        self.grid[PIT2[0]][PIT2[1]] = 'P'
-        self.grid[self.agent_location[0]][self.agent_location[1]] = 'A'
+    def is_valid_move(self, pos):
+        x, y = pos
+        return 0 <= x < self.size and 0 <= y < self.size
 
     def move_agent(self, direction):
-        if self.game_over:
-            print("Igra je završena.")
-            return
+        x, y = self.agent_pos
+        if direction == 'up':
+            new_pos = (x - 1, y)
+        elif direction == 'down':
+            new_pos = (x + 1, y)
+        elif direction == 'left':
+            new_pos = (x, y - 1)
+        elif direction == 'right':
+            new_pos = (x, y + 1)
+        else:
+            return False
 
-        row, col = self.agent_location
-
-        if direction == 'up' and row > 0:
-            self.agent_location = (row - 1, col)
-        elif direction == 'down' and row < ROWS - 1:
-            self.agent_location = (row + 1, col)
-        elif direction == 'left' and col > 0:
-            self.agent_location = (row, col - 1)
-        elif direction == 'right' and col < COLS - 1:
-            self.agent_location = (row, col + 1)
-
-        self.check_environment()
-
-    def check_environment(self):
-        if self.agent_location == WUMPUS and self.agent_alive:
-            self.agent_alive = False
-            self.game_over = True
-            print("Agent je umro. Igra je završena.")
-        elif self.agent_location == GOLD and self.agent_alive:
-            self.agent_won = True
-            self.game_over = True
-            print("Agent je pronašao zlato. Pobijedio je!")
-        elif self.agent_location == PIT1 or self.agent_location == PIT2:
-            self.agent_alive = False
-            self.game_over = True
-            print("Agent je pao u jamu. Igra je završena.")
-
-        self.update_grid()
-
-    def find_path_dfs(self):
-        visited = set()
-        path = []
-        self.dfs(self.agent_location, visited, path)
-        return path
-
-    def dfs(self, current_location, visited, path):
-        if current_location == GOLD:
+        if self.is_valid_move(new_pos):
+            self.agent_pos = new_pos
+            self.handle_encounter()
             return True
-
-        visited.add(current_location)
-
-        neighbors = self.get_valid_neighbors(current_location)
-        for neighbor in neighbors:
-            if neighbor not in visited:
-                path.append(neighbor)
-                if self.dfs(neighbor, visited, path):
-                    return True
-                path.pop()
-
         return False
 
-    def get_valid_neighbors(self, location):
-        row, col = location
-        neighbors = []
+    def handle_encounter(self):
+        x, y = self.agent_pos
+        if self.agent_pos == self.wumpus_pos:
+            print("Game over! You were eaten by the Wumpus.")
+            self.initialize_game()
+        elif self.agent_pos == self.pit_pos:
+            print("Game over! You fell into a pit.")
+            self.initialize_game()
+        elif self.agent_pos == self.gold_pos:
+            print("Congratulations! You found the gold.")
+            self.initialize_game()
+        elif self.wumpus_pos in self.get_adjacent_cells(self.agent_pos):
+            print("You smell a terrible stench.")
+        elif self.pit_pos in self.get_adjacent_cells(self.agent_pos):
+            print("You feel a cool breeze.")
 
-        if row > 0:
-            neighbors.append((row - 1, col))
-        if row < ROWS - 1:
-            neighbors.append((row + 1, col))
-        if col > 0:
-            neighbors.append((row, col - 1))
-        if col < COLS - 1:
-            neighbors.append((row, col + 1))
+    def display_grid(self):
+        for i in range(self.size):
+            print("+" + "---+" * self.size)  # Horizontal line
 
-        return neighbors
+            for j in range(self.size):
+                if (i, j) == self.agent_pos:
+                    print("| A ", end="")
+                elif (i, j) == self.wumpus_pos:
+                    print("| W ", end="")
+                elif (i, j) == self.pit_pos:
+                    print("| P ", end="")
+                elif (i, j) == self.gold_pos:
+                    print("| G ", end="")
+                elif (i, j) in self.get_adjacent_cells(self.pit_pos): #or (i, j) in self.get_adjacent_cells(self.wumpus_pos):
+                    print("| B ", end="")
+                else:
+                    print("|   ", end="")
+            print("|")  # End of row
 
-    def automate_moves(self):
-        if self.game_over:
-            print("Igra je završena.")
-            return
+        print("+" + "---+" * self.size)  # Final horizontal line
 
-        path = self.find_path_dfs()
 
-        for next_location in path:
-            self.agent_location = next_location
-            self.check_environment()
 
-            if self.game_over:
-                break
 
-            self.print_grid()
+# Testing the game
+game = WumpusGame()
+game.initialize_game()
+game.display_grid()
 
-game = WumpusWorld()
-game.update_grid()
-
-while not game.game_over:
-    game.print_grid()
-
-    automate = input("Automatski pokreti? (da/ne): ")
-    if automate.lower() == 'da':
-        game.automate_moves()
+while True:
+    direction = input("Enter your move (up, down, left, right): ")
+    if game.move_agent(direction.lower()):
+        game.display_grid()
     else:
-        move = input("Unesite potez (up/down/left/right): ")
-        game.move_agent(move)
+        print("Invalid move. Try again.")
